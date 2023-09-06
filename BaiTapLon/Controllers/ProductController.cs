79a06c4ce051fa871a5b94ea413ccde6885c826b
1951052175TranDucTai <1951052175tai@ou.edu.vn>
2023-09-06 19:30:22 +0700

    BaiTapLon

--- BaiTapLon/Controllers/ProductController.cs
+++ BaiTapLon/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using BaiTapLon.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaiTapLon.Controllers
 {
@@ -17,23 +19,36 @@ namespace BaiTapLon.Controllers
         // GET: ProductController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Product p = da.Products.Where(s => s.ProductId == id).FirstOrDefault();
+            return View(p);
         }
 
         // GET: ProductController/Create
         public ActionResult Create()
         {
+            ViewData["Brand"] = new SelectList(da.Brands, "BrandID", "BrandName");
+            ViewData["Category"] = new SelectList(da.Categories, "CategoryID", "CategoryName");
+
             return View();
         }
 
         // POST: ProductController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Product product, IFormCollection collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                // TODO: Add insert logic here
+                Product p = new Product();
+                p = product;
+                p.CategoryId = int.Parse(collection["Category"]);
+                p.BrandId = int.Parse(collection["Brand"]);
+
+                da.Products.Add(p);
+                da.SaveChanges();
+
+                return RedirectToAction("ListProducts");
             }
             catch
             {
@@ -44,17 +59,37 @@ namespace BaiTapLon.Controllers
         // GET: ProductController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Product p = da.Products.FirstOrDefault(s => s.ProductId == id);
+
+            ViewData["Brand"] = new SelectList(da.Brands, "BrandID", "BrandName");
+            ViewData["Category"] = new SelectList(da.Categories, "CategoryID", "CategoryName");
+
+            return View(p);
         }
 
         // POST: ProductController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Product product, FormCollection collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                // TODO: Add update logic here
+                Product p = da.Products.First(s => s.ProductId == product.ProductId);
+                p.ProductId = product.ProductId;
+                p.ProductName = product.ProductName;
+                p.Price = product.Price;
+                p.UnitsInStock = product.UnitsInStock;
+                p.Quantity = product.Quantity;
+
+                
+
+                p.CategoryId = int.Parse(collection["Category"]);
+                p.BrandId = int.Parse(collection["Brand"]);
+
+                da.SaveChanges();
+
+                return RedirectToAction("ListProducts");
             }
             catch
             {
@@ -65,17 +100,23 @@ namespace BaiTapLon.Controllers
         // GET: ProductController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Product p = da.Products.FirstOrDefault(s => s.ProductId == id);
+            return View(p);
         }
 
-        // POST: ProductController/Delete/5
+        // POST: Product/Delete/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Product product)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                // TODO: Add delete logic here
+                Product p = da.Products.First(s => s.ProductId == id);
+
+                da.Products.Remove(p);
+                
+
+                return RedirectToAction("ListProducts");
             }
             catch
             {
